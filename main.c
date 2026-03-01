b85306f4e6de7b0c3a7efd14582c62a2cfeec9ee
marun2108-byte <marun2108@gmail.com>
2026-03-01 18:53:30 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -2,8 +2,19 @@
 #include <stdlib.h>
 
 int main() {
+    setbuf(stdout, NULL);   // Disable buffering (VERY IMPORTANT for browser)
 
-            printf("Invalid input or cancelled.\n");
-            return 0;
+    char name[50];
+
+    printf("Enter your name: ");
+    fflush(stdout);
+
+    if (scanf("%49s", name) != 1) {
+        printf("Invalid input.\n");
+        return 0;
+    }
+
+    printf("Hello, %s!\n", name);
+
     
 }
